c7503020f3aa673a64748bad6bb53e9ea52eb2d5
tkxksdl2 <tkxksdl2@gmail.com>
2021-07-12 11:10:24 +0900

    07/08 6ban class commit 2. get/ post

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,8 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def hello_world(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
         return render(request, 'accountapp/hello_world.html/',
-                      context={'text': 'POST METHOD!'})
+                      context={'text': temp})
     else:
         return render(request, 'accountapp/hello_world.html/',
                       context={'text': 'GET METHOD!'})
