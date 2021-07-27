1c4bd1e6ce206be41e163271fe123f23c2cbc2ff
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-07-27 17:31:18 +0900

    07/27 6ban class commit 1. ProfileCreation Form

--- profileapp/models.py
+++ profileapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
-                                related_name='porfile')
+                                related_name='profile')
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=30, unique=True, null=True)
     message = models.CharField(max_length=200, null=True)
