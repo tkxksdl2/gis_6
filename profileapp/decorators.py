266f6d8ceebe3d54b460c72335a22da5c03a0b1a
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-07-29 11:49:09 +0900

    07/29 6ban class commit 1. Profile ownership required

--- profileapp/decorators.py
+++ profileapp/decorators.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponseForbidden
+from profileapp.models import Profile
 
 
 def profile_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        from profileapp.models import Profile
         target_profile = Profile.objects.get(pk=kwargs['pk'])
         if request.user == target_profile.user:
             return func(request, *args, **kwargs)
