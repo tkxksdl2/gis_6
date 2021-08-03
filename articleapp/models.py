d3297dc9bc62469ae9b2361c3498b04a76684c99
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-08-03 16:51:50 +0900

    08/03 6ban class commit 0. Article Create

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Article(models.Model):
+
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)
     title = models.CharField(max_length=200, null=True)
