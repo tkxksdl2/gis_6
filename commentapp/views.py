2e16d274ff79277eb69c48194daaac79831b3f22
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-08-09 12:38:33 +0900

    08/09 6ban class commit 5. commentapp

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import CreateView
+from django.urls import reverse
+from django.views.generic import CreateView, DeleteView
 
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
@@ -19,4 +20,13 @@ class CommentCreateView(CreateView):
 
     def get_success_url(self):
         from django.urls import reverse
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_content'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
