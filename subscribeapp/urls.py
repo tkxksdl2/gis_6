308576183736ddee7454d4f60551f5bf4b59de05
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-08-23 11:24:01 +0900

    08/23 6ban class commit 1. projectapp

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,5 +1,10 @@
+from django.urls import path
+
+from subscribeapp.views import SubscriptionView
+
+app_name = 'subscription'
 
 urlpatterns = [
-
+    path('subcribe/<int:project_pk>', SubscriptionView.as_view(), name='subscription'),
 
 ]
