f6c340980bc0312ad3b7ca24ddd1f71e9ccc0dd4
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-08-30 11:18:17 +0900

    08/30 6ban class commit 1.

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -18,7 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from articleapp.views import ArticleListView
+
 urlpatterns = [
+    path('', ArticleListView.as_view(), name='home'),
+
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profile/', include('profileapp.urls')),
@@ -26,6 +30,6 @@ urlpatterns = [
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
     path('subscribe/', include('subscribeapp.urls')),
-    path('likes/', include('likeapp.urls'))
+    path('likes/', include('likeapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
